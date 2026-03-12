a7d6caa498777a1856a8fdd876798a7521c6b53a
Liene31 <67922137+Liene31@users.noreply.github.com>
2026-03-12 11:15:54 +0100

    sketch the general idea of routes and backend architecture

--- server.js
+++ server.js
@@ -4,10 +4,19 @@ const app = express();
 
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send({ message: "OK, 200" });
-});
-
 app.listen(PORT, () => {
   console.log(`Server runs on port ${PORT}`);
 });
+
+// in SERVER
+// app.get("/") -> serve frontend
+
+// in ROUTER
+// /api/books -> CRUD
+// /api/auth
+// /api/users
+
+//for searching and filtering currently will leave in front
+//if time, can implement it backend
+//my DB currently is small, fetching all data and them filtering is okay
+//later when DB becomes bigger, more efficient is to serve only what's requested
