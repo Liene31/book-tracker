bc0804855529c6043000f8f2dc3ea3f7c6a704b3
Liene31 <67922137+Liene31@users.noreply.github.com>
2026-03-15 12:00:59 +0100

    create book schema and connection to Mongo DB

---
+++ models/book.model.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const { Schema, model, Types } = mongoose;
+
+const bookSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    author: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    status: {
+      type: String,
+    },
+
+    rating: {
+      type: Number,
+      max: 5,
+    },
+    description: {
+      type: String,
+    },
+    coverUrl: {
+      type: String,
+    },
+  },
+  {
+    collection: "Book",
+    timestamps: true,
+  },
+);
+
+export const Book = model("Book", bookSchema);
--- server.js
+++ server.js
@@ -2,18 +2,34 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import { router } from "./routes/index.js";
+import { connect } from "http2";
+import mongoose from "mongoose";
 
 const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
 
-const { PORT } = process.env;
+const { PORT, DB_CONNECTION } = process.env;
 
 const app = express();
 
 app.use(express.json());
 //remove ../public from index.html
 app.use(express.static(path.join(__dirname, "public")));
+
+//Important to connect to DB before router
+app.use(async (req, res, next) => {
+  try {
+    await mongoose.connect(DB_CONNECTION, { dbName: "BookTracker" });
+    console.log("Successfully connected to the DB");
+    next();
+  } catch (error) {
+    console.log(`Connection failed with error ${error}`);
+    res
+      .status(500)
+      .json({ statusCode: 500, message: "Impossible to connect to DB" });
+  }
+});
 
 app.use("/api", router);
 
